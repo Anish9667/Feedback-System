2f83a86ccd843048ac8ada2197108bd9b2049b6d
anishpal9667 <anishpal9667@gmail.com>
2025-06-13 11:28:59 +0530

    starting setup

--- backend/server.js
+++ backend/server.js
@@ -11,19 +11,19 @@ app.use(cors());
 app.use(express.json()); 
 
 app.get("/", (req, res) => {
-  res.send("Feedback System API Running ✅");
+  res.send("Feedback System API Running ");
 });
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log("MongoDB Connected ✅");
+  console.log("MongoDB Connected ");
 
   app.listen(process.env.PORT, () => {
     console.log(`Server Running on port ${process.env.PORT}`);
   });
 
 }).catch((err) => {
-  console.error("MongoDB Connection Failed ❌", err);
+  console.error("MongoDB Connection Failed ", err);
 });
