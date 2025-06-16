4f8a4a7d67d0448c96f6eb32760e67c327c30176
anishpal9667 <anishpal9667@gmail.com>
2025-06-16 12:41:53 +0530

    recheck

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,20 +1,8 @@
-// import axios from 'axios';
 
-// const instance = axios.create({
-//   baseURL: 'http://localhost:5000/api', 
-// });
-
-// instance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default instance;
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ✅ dynamic base URL
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 instance.interceptors.request.use((config) => {
